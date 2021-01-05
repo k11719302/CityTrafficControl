a54fa8b8cde6d67a8d74b39969a67511f882f1ad
k11719302 <62445393+k11719302@users.noreply.github.com>
2021-01-05 17:20:14 +0100

    UnitTest-Project //General +basic test project structure +UnitTestTemplate class

---
+++ CityTrafficControlTests/UnitTestTemplate.cs
@@ -0,0 +1,32 @@
+﻿using CityTrafficControl;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+// See: https://docs.microsoft.com/en-us/visualstudio/test/walkthrough-creating-and-running-unit-tests-for-managed-code?view=vs-2019
+
+namespace CityTrafficControlTests {
+	[TestClass]
+	public class UnitTestTemplate {
+		[TestMethod]
+		public void Test_NormalAdditionTest() {
+			// arrange/initialize
+			int a = 2, b = 3, actual;
+			int expected = 5;
+
+			// act/run
+			actual = a + b;
+
+			// assert/check
+			Assert.AreEqual(expected, actual, "Addition of 2 and 3 should be 5");
+		}
+
+		[TestMethod]
+		public void Test_DivisionByZeroException() {
+			// arrange/initialize
+			int a = 2, b = 0, result;
+
+			// act/run + assert/check
+			Assert.ThrowsException<DivideByZeroException>(() => result = a / b);
+		}
+	}
+}
