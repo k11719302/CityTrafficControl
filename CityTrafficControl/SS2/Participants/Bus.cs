936d95480cbeb3e6af68cec8bb47d55480e8496a
k11719302 <62445393+k11719302@users.noreply.github.com>
2021-01-09 15:18:26 +0100

    Declare abstract methods to be buildable
    //SS2
    # implemented abstract methods with NotImplementedExceptions

--- CityTrafficControl/SS2/Participants/Bus.cs
+++ CityTrafficControl/SS2/Participants/Bus.cs
@@ -3,8 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CityTrafficControl.Master.StreetMap;
+using CityTrafficControl.SS2.Simulation;
 
 namespace CityTrafficControl.SS2.Participants {
 	class Bus : PublicTransport {
+		protected override void dropPassengers(PublicTransportStation station) {
+			throw new NotImplementedException();
+		}
+
+		protected override void SimulationManager_SimulateTick(object sender, SimulationEventArgs e) {
+			throw new NotImplementedException();
+		}
+
+		protected override void takePassengers(List<Pedestrian> newPassengers) {
+			throw new NotImplementedException();
+		}
 	}
 }
